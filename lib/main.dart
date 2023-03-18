6ca4773489be58341cc92f632fb1666ebcf9db5b
krishnaji <prahaladsharma4u@gmail.com>
2023-03-18 15:17:07 +0530

    new commit

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:alert/alert.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
+
+void main() {
+  runApp( Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizePage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizePage extends StatefulWidget {
+  const QuizePage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizePage> createState() => _QuizePageState();
+}
+
+class _QuizePageState extends State<QuizePage> {
+
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickerAnswer){
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    setState(() {
+
+      if(quizBrain.isFinished() == true){
+        Alert(message: 'Your question is endup').show();
+
+        quizBrain.reset();
+
+        scoreKeeper = [];
+
+      } else {
+        if(userPickerAnswer == correctAnswer){
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+        } else{
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+        }
+
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey)),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: (){
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey)),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: (){
+                checkAnswer(false);
+              },
+            ),
+        ),),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
+
